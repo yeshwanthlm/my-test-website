e04d3dc7d19fa9f85ca1ecb0fbe15ca340452992
Yeshwanth L M <yeshwanthlm@Yeshwanths-MacBook-Pro.local>
2024-10-18 21:05:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    const message = document.getElementById('message');
+    message.innerText = "Welcome to the peaceful world of cloud computing!";
+    message.style.visibility = 'visible';
+  }
+  
